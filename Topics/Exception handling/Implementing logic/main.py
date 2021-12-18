000c7a3843146fa397e3971cee7cc1a2b6e763fd
liuyunan <liuyunan.20@gmail.com>
2021-12-17 22:38:28 -0800

    just assert follow try is enough

--- Topics/Exception handling/Implementing logic/main.py
+++ Topics/Exception handling/Implementing logic/main.py
@@ -1,8 +1,7 @@
 full_name = input().split(' ')
 try:
-    l = len(full_name)
-    assert l == 2
-except AssertionError as error:
+    assert len(full_name) == 2
+except AssertionError:
     print("You need to enter exactly 2 words. Try again!")
 else:
     print(f"Welcome to our party, {full_name[0]} {full_name[1]}")
