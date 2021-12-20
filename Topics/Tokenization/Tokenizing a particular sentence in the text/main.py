2c86bee452a30253b2e11165072c2030fcf42e8f
liuyunan <liuyunan.20@gmail.com>
2021-12-20 01:03:32 -0800

    tokenize a text to sentences then to words with regexp

---
+++ Topics/Tokenization/Tokenizing a particular sentence in the text/main.py
@@ -0,0 +1,8 @@
+from nltk.tokenize import regexp_tokenize
+from nltk.tokenize import sent_tokenize
+text = input()
+sentences = sent_tokenize(text)
+n = int(input())
+print(regexp_tokenize(sentences[n], "[A-z']+"))
+
+
