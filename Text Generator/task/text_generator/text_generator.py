b86fbde17f37af6aaafb5bd6068c3a08467d9cc4
liuyunan <liuyunan.20@gmail.com>
2021-12-22 01:46:34 -0800

    can't create markov dict for every head and it's tails

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,23 +1,36 @@
 from nltk.tokenize import regexp_tokenize
 from nltk.tokenize import WhitespaceTokenizer
+from collections import Counter
 f = open(f'/Users/liudawei/PycharmProjects/Text Generator/Text Generator/task/{input()}', "r", encoding="utf-8")
 wt = WhitespaceTokenizer()
 words = wt.tokenize(f.read())
+
 bigrams = []
 for x in range(0, len(words) - 1):
     bigrams.append([words[x], words[x + 1]])
 
-print(f"Number of bigrams: {len(bigrams)}")
+uniq_words = set()
+for x in words:
+    uniq_words.add(x)
+# print(uniq_words)
+markov_dict = {}
+for x in uniq_words:
+    for bigram in bigrams:
+        if x == bigram[0]:
+            markov_dict.setdefault(x, []).append(bigram[1])
+print(markov_dict)
+
+def find_tails(head):
+    tail_list = markov_dict[head]
+    if tail_list is None:
+        print("Key Error. The requested word is not in the model. Please input another word.")
+    else:
+        freq = dict(Counter(tail_list))
+        for tail in freq:
+            print(f"Tail: {tail}    Count: {freq[tail]}")
+
 
 user_in = input()
 while user_in != "exit":
-    try:
-        n = int(user_in)
-    except ValueError:
-        print("Type Error. Please input an integer.")
-    else:
-        if int(n) < len(bigrams):
-            print(f"Head: {bigrams[int(n)][0]} Tail: {bigrams[int(n)][1]}")
-        else:
-            print("Index Error. Please input an integer that is in the range of the corpus.")
+    find_tails(user_in)
     user_in = input()
