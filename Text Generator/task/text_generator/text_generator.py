7ce0c3783902b770790ed311f649b3ba0d35b2c5
liuyunan <liuyunan.20@gmail.com>
2021-12-22 23:11:09 -0800

    generate a sentence using random.choices method to predict a word based on previous word

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,30 +1,21 @@
-from nltk.tokenize import regexp_tokenize
+import random
 from nltk.tokenize import WhitespaceTokenizer
 from collections import Counter
 f = open(f'/Users/liudawei/PycharmProjects/Text Generator/Text Generator/task/{input()}', "r", encoding="utf-8")
 wt = WhitespaceTokenizer()
 words = wt.tokenize(f.read())
 
-
 markov_dict = {}
 for x in range(0, len(words) - 1):
     markov_dict.setdefault(words[x], []).append(words[x + 1])
-# print(markov_dict)
 
-
-def find_tails(head):
-    try:
-        tail_list = markov_dict[head]
-    except KeyError:
-        print("Key Error. The requested word is not in the model. Please input another word.")
-    else:
+for i in range(10):
+    prev_word = random.choice(words)
+    sentence = [prev_word]
+    for n in range(9):
+        tail_list = markov_dict[prev_word]
         freq = dict(Counter(tail_list))
-        for tail in freq:
-            print(f"Tail: {tail}    Count: {freq[tail]}")
-
-
-user_in = input()
-while user_in != "exit":
-    print(f"Head: {user_in}")
-    find_tails(user_in)
-    user_in = input()
+        cur_word = random.choices(list(freq.keys()), weights=tuple(freq.values()))
+        sentence.append(cur_word[0])
+        prev_word = cur_word[0]
+    print(" ".join(sentence))
